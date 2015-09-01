e0b2657dc0ccfbc5b41d1137f1bc42139a5e0682
Miwand <miwand.najafe@gmail.com>
2015-08-31 20:51:26 -0400

    initial commit

---
+++ TriviallyPursued_1/TriviallyPursued_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TriviallyPursued_1
+//
+//  Created by Miwand Najafe on 2015-08-31.
+//  Copyright (c) 2015 Miwand Najafe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
