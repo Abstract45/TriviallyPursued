2477d95b7a6e0f3aa223de8f32eeddced3b8fff2
Miwand <miwand.najafe@gmail.com>
2015-08-31 23:30:50 -0400

    Trivially_pursuit_part1

---
+++ TriviallyPursued_1/TriviallyPursued_1/MyFirstStruct.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstStruct.swift
+//  TriviallyPursued_1
+//
+//  Created by Miwand Najafe on 2015-08-31.
+//  Copyright (c) 2015 Miwand Najafe. All rights reserved.
+//
+
+import Foundation
+
+struct Trivia {
+    var name:String = ""
+    var country:String = ""
+    var trivia:String = ""
+}
+
+
--- TriviallyPursued_1/TriviallyPursued_1/ViewController.swift
+++ TriviallyPursued_1/TriviallyPursued_1/ViewController.swift
@@ -10,8 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//          var scene = Trivia(name:"CN Tower",country:"Canada",trivia:"Large Building")
+    
+    var scene = [
+        Trivia(name:"CN Tower",country:"Canada",trivia:"Large Building"),
+        Trivia(name:"Leaning tower of piza",country:"Italy",trivia:"Slanted Building"),
+        Trivia(name:"Eiffel tower",country:"Paris",trivia:"Interesting Building"),
+        Trivia(name:"Petronas Tower",country:"Malaysia",trivia:"An iconic land mark"),
+        Trivia(name:"White house",country:"USA",trivia:"Where the president lives"),
+        Trivia(name:"St Basil's Cathedral",country:"Russia",trivia:"Beautiful church"),
+        Trivia(name:"Colosseum",country:"Italy",trivia:"largest Roman amphitheatre"),
+        Trivia(name:"Taj Mahal",country:"India",trivia:"white marble mausoleum"),
+        Trivia(name:"Sydney Opera House",country:"Australia",trivia:"most famous Australian architectural icon"),
+        Trivia(name:"Hagia Sophia",country:"Turkey",trivia:"Once a church, later a mosque, and now a museum")
+        
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        
+        
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
